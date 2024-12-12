1906efaca5607808a1ccd3b5a399d2da88091e6d
Ramesh Jadav <rameshjaav@gmail.com>
2024-12-12 13:39:41 +0530

    updated home file

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,19 +4,19 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { computed } from "vue";
 const store = useStore();
 const user = computed(() => store.state.user);
-import BarChart from '../components/Miscs/BarChart.vue';
+import BarChart from "../components/Miscs/BarChart.vue";
 </script>
-
 
 <template>
     <AuthenticatedLayout v-if="user">
-        <div>
-            <h1>Welcome, {{ user.name }}</h1>
-            <p>Email: {{ user.email }}</p>
-        </div>
-
         <div class="row">
-            <div class="col-md-12">
+
+            <div class="col-md-12 mb-3">
+                <h1>Welcome, {{ user.name }}</h1>
+                <p>Email: {{ user.email }}</p>
+            </div>
+
+            <div class="col-md-6 mb-3">
                 <BarChart />
             </div>
         </div>
